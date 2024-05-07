88c127225324da666c2ffd98a88760b42226035b
mA4rK0 <162128290+mA4rK0@users.noreply.github.com>
2024-05-07 09:46:47 +0700

    Add files via upload

---
+++ DOM Manipulation 1/script.js
@@ -0,0 +1,12 @@
+// const judul = document.getElementById("judul");
+// judul.innerHTML = "<em>you</em>";
+
+// const sectionA = document.querySelector("section#a");
+// sectionA.innerHTML = "okay";
+
+// const judul = document.querySelector("#judul");
+// judul.style.color = "yellow";
+// judul.style.backgroundColor = "blue";
+
+const judul = document.getElementsByTagName("h1")[0];
+judul.setAttribute("name", "boy");
